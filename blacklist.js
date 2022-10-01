15324b36768a0d5c7b4e5792f4e9c6b4a3d13cfc
imtherainman <15202629+imtherainman@users.noreply.github.com>
2022-10-01 00:36:01 -0700

    allow unknown domains for podcasts

--- blacklist.js
+++ blacklist.js
@@ -1,6 +1,9 @@
 const blacklist = [
   "https://spclient.wg.spotify.com/ads/**",
-  "https://spclient.wg.spotify.com/ad-logic/*"
+  "https://spclient.wg.spotify.com/ad-logic/**",
+  "https://spclient.wg.spotify.com/gabo-receiver-service/**",
+  "**/*sentry.io*/**",
+  "**/*googleapis*/**",
 ];
 
 module.exports = blacklist;
