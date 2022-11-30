b73eaa08ea7614c5452a35e9a6f7c1a680afcbaa
imtherainman <15202629+imtherainman@users.noreply.github.com>
2022-11-30 12:13:26 -0800

    add spclient.wg.spotify.com/sponsoredplaylist to block list

--- blacklist.js
+++ blacklist.js
@@ -2,6 +2,7 @@ const blacklist = [
   "https://spclient.wg.spotify.com/ads/**",
   "https://spclient.wg.spotify.com/ad-logic/**",
   "https://spclient.wg.spotify.com/gabo-receiver-service/**",
+  "https://spclient.wg.spotify.com/sponsoredplaylist/**",
   "**/*sentry.io*/**",
   "**/*googleapis*/**",
   "**/*doubleclick*/**",
